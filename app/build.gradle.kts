69b8be1eafe6f82e3181a9f39222a1ded9f13ae7
DHR60 <192860629+DHR60@users.noreply.github.com>
2026-07-23 12:13:53 +0800

    doc. bump version `v0.0.2`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 29
         targetSdk = 37
         versionCode = 1
-        versionName = "0.0.1"
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
